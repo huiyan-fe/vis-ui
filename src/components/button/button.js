4eee9b8f6a3fe5ef8cbdee856e66623b3e0438bc
Junior2ran <hdr01@126.com>
2018-06-07 16:49:05 +0800

    feat(palette): complete color palette

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,9 +1,15 @@
 import React from 'react';
-// import './style/index';
+import classNames from "classnames";
+import './style/index';
 
-export default class Button extends React.Component {
+class Button extends React.Component {
     render() {
-        const classname = this.props.className;
+        const classname = classNames('visui-btn', this.props.className);
         return <div className={classname}>{this.props.children}</div>;
     }
 }
+
+Button.defaultProps = {};
+Button.propTypes = {};
+
+export default Button;
