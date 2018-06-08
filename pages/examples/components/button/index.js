5fd92bb4c5d5ee824fb575ba012f51aa49be37f9
Junior2ran <hdr01@126.com>
2018-06-08 14:37:59 +0800

    feat(button group): complete ButtonGroup component

--- pages/examples/components/button/index.js
+++ pages/examples/components/button/index.js
@@ -5,39 +5,41 @@ const ButtonGroup = Button.Group;
 export default class App extends Component {
     render() {
         return <div>
+            <Button>按钮default</Button>
+            <Button size="small">按钮small</Button>
+            <Button size="large">按钮large</Button>
+            <Button disabled>按钮disabled</Button>
+            <br/><br/>
+            <Button type="primary">按钮primary</Button>
+            <Button type="primary" size="small">按钮primary</Button>
+            <Button type="primary" size="large">按钮primary</Button>
+            <Button type="primary" disabled>按钮disabled</Button>
+            <br/><br/>
+            <Button type="ghost">按钮ghost</Button>
+            <Button type="ghost" size="small">按钮ghost</Button>
+            <Button type="ghost" size="large">按钮ghost</Button>
+            <Button type="ghost" disabled>按钮ghost</Button>
+            <br/><br/>
+            <Button type="danger">按钮danger</Button>
+            <Button type="danger" size="small">按钮danger</Button>
+            <Button type="danger" size="large">按钮danger</Button>
+            <Button type="danger" disabled>按钮danger</Button>
+            <br/><br/>
+            <Button type="success">按钮success</Button>
+            <Button type="success" size="small">按钮success</Button>
+            <Button type="success" size="large">按钮success</Button>
+            <Button type="success" disabled>按钮success</Button>
+            <br/><br/>
+            <Button type="primary" onClick={()=>{alert('haha');}}>点击onClick出弹窗</Button>
+            <br/><br/>
+            <h2>按钮组 Button.Group</h2>
             <ButtonGroup>
-                <Button>按钮default</Button>
-                <Button size="small">按钮small</Button>
-                <Button size="large">按钮large</Button>
-                <Button disabled>按钮disabled</Button>
+                <Button type="primary">按钮1</Button>
+                <Button type="primary">按钮2</Button>
+                <Button type="primary">按钮3</Button>
+                <Button type="primary">按钮4</Button>
             </ButtonGroup>
-            <ButtonGroup>
-                <Button type="primary">按钮primary</Button>
-                <Button type="primary" size="small">按钮primary</Button>
-                <Button type="primary" size="large">按钮primary</Button>
-                <Button type="primary" disabled>按钮disabled</Button>
-            </ButtonGroup>
-            <ButtonGroup>
-                <Button type="ghost">按钮ghost</Button>
-                <Button type="ghost" size="small">按钮ghost</Button>
-                <Button type="ghost" size="large">按钮ghost</Button>
-                <Button type="ghost" disabled>按钮ghost</Button>
-            </ButtonGroup>
-            <ButtonGroup>
-                <Button type="danger">按钮danger</Button>
-                <Button type="danger" size="small">按钮danger</Button>
-                <Button type="danger" size="large">按钮danger</Button>
-                <Button type="danger" disabled>按钮danger</Button>
-            </ButtonGroup>
-            <ButtonGroup>
-                <Button type="success">按钮success</Button>
-                <Button type="success" size="small">按钮success</Button>
-                <Button type="success" size="large">按钮success</Button>
-                <Button type="success" disabled>按钮success</Button>
-            </ButtonGroup>
-            <ButtonGroup>
-                <Button type="primary" onClick={()=>{alert('haha');}}>点击onClick出弹窗</Button>
-            </ButtonGroup>
+            <br/>
         </div>;
     }
 }
--- src/components/button/docs/index.md
+++ src/components/button/docs/index.md
@@ -2,14 +2,17 @@
 
 ## 说明
 单独的`<Button>`没有外边距属性。
-将`<Button>`嵌套在`<Button.Group>`中，会使内部的`<Button>`具有向右`8px`的外边距，同时`<Button.Group>`自身具有向下`12px`的外边距。
+
+当`<Button>`与`<Button>`相邻时，会使相邻的后一个`<Button>`具有向左`8px`的外边距。
+
+将`<Button>`嵌套在`<Button.Group>`容器中，便于大量`<Button>`组件的管理，同时`<Button.Group>`自身具有向下`12px`的外边距。
 
 ## API
 
 | 属性 | 说明 | 类型 | 可选值 | 默认值 |
 | --- | --- | --- | --- | --- |
 | type | 设置按钮类型 | string | `default` `primary` `ghost` `danger` `success` | `default` |
-| size | 设置按钮大小 | string | `small` | - |
+| size | 设置按钮大小 | string | `large` `small` | - |
 | icon | 设置按钮的图标类型 | string | 见`Icon`组件 | - |
 | disabled | 按钮失效状态 | boolean | `true` `false` | `false` |
 | onClick | `click` 事件的 handler | function | - | - |
