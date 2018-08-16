d5e670f39099bb22da40f505ff4a87a22f36f1b0
Junior2Ran <hdr01@126.com>
2018-08-16 14:57:57 +0800

    feat(Message): 增加Message组件显示时长参数

--- src/components/message/message.js
+++ src/components/message/message.js
@@ -5,12 +5,12 @@ div.className = 'visui-message';
 var timeout = null;
 
 export default {
-    tip: function (msg) {
+    tip: function (msg, time) {
         clearTimeout(timeout);
         div.innerHTML = msg;
         document.body.appendChild(div);
         timeout = setTimeout(() => {
             document.body.removeChild(div);
-        }, 2000);
+        }, time || 2000);
     }
 }
